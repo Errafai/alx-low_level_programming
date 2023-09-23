2d5cc7c35ada45f2dcf147207877274cca046c86
Errafai <errafaiachraf1923@gmail.com>
2023-09-23 21:23:50 +0000

    task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,27 @@
 #include <stdlib.h>
 #include "lists.h"
 #include <string.h>
+
+#include "lists.h"
+
+/**
+ * *_strlen - calcule le long d'un string
+ * *@str: the string
+ * *Return: the length of the string
+ * */
+
+int _strlen(const char *str)
+{
+	        int i = 0;
+
+		        while(str[i])
+				                i++;
+			        return (i);
+}
+
+
+
+
 /**
 *add_node - adds a new node at the beginning of a list
 *@head: the head of the list
