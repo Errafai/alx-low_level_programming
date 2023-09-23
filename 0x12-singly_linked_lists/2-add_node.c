151ba6cc4fc512d0157b5f0df3505aadf589574a
Errafai <errafaiachraf1923@gmail.com>
2023-09-23 21:29:00 +0000

    task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,18 +5,17 @@
 #include "lists.h"
 
 /**
- * *_strlen - calcule le long d'un string
- * *@str: the string
- * *Return: the length of the string
- * */
-
+ *_strlen - calcule le long d'un string
+ *@str: the string
+ *Return: the length of the string
+ */
 int _strlen(const char *str)
 {
-	        int i = 0;
+	int i = 0;
 
-		        while(str[i])
-				                i++;
-			        return (i);
+	while (str[i])
+		i++;
+	return (i);
 }
 
 
