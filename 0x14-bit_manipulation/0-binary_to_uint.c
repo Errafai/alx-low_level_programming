066376a3d48f3662417cdee88f22323e576b4626
Errafai <errafaiachraf1923@gmail.com>
2023-09-28 20:52:05 +0000

    task0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert a binary number to unsigned int
+ * @b: the binary number
+ * REturn: the unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i;
+
+	for (i = 0; b && *b; b++)
+	{
+		if (*b != '0' || *b != '1')
+			return (0);
+		i <<= 1;
+		i += *b -'0';
+	}
+	return (i);
+}
