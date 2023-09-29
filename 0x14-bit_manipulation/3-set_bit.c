fb56517cada58e905d293634198311b51975397d
Errafai <errafaiachraf1923@gmail.com>
2023-09-29 14:52:02 +0000

    task3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: the number
+ * @index: the index of the bit
+ * Return: return 1 in succes of -1 in fail
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > 64)
+		return (-1);
+	i = *n;
+	*n = (((i >> index) | 1 ) << index);
+	return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdlib.h>
+
+
+int set_bit(unsigned long int *n, unsigned int index);
+
+
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
