7b70b3a4cb9dfeece93861e7a2c07a0e0f987b55
Errafai <errafaiachraf1923@gmail.com>
2023-09-29 16:26:13 +0000

    task3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i;
 
-	if (index > sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	i = *n;
 	*n = (((i >> index) | 1) << index);
