0df462182fd1f24a5d78a3d543111579bffa9e2a
Errafai <errafaiachraf1923@gmail.com>
2024-01-26 12:57:58 +0100

    7-insert_dnodeint.c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,73 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - add a node at the idx
+ * @idx: the index of the place of the new node
+ * @h: the head of the list
+ * @n: the number (data)
+ * Return: the new node and NULL if it faild
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *nth;
+	unsigned int j;
+
+	j = listint_len(*h) - 1;
+	if (idx > j)
+		return (NULL);
+	if (idx == 0 || h == NULL)
+		new = add_dnodeint(h, n);
+	else if (idx == j)
+		new = add_dnodeint_end(h, n);
+	else
+	{
+		nth = get_dnodeint_at_index(*h, idx);
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		nth->prev->next = new;
+		new->prev = nth->prev;
+		new->next = nth;
+		nth->prev = new;
+	}
+	return (new);
+}
+
+/**
+ * dlistint_len - #include "lists.h"
+ * @h: the head of the DDl
+ * Return: the number of nodes in DDL
+ */
+size_t listint_len(dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+/**
+ * get_dnodeint_at_index - get the nth node from the list
+ * @head: the head of the list
+ * @index: the index of the nth node
+ * Return: the nth node and NULL if the node dont exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0, j;
+	dlistint_t *curr;
+
+	j = listint_len(head) - 1;
+	if (index > j || !head)
+		return (NULL);
+	curr = head;
+	while (i != index)
+	{
+		curr = curr->next;
+		i++;
+	}
+	return (curr);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct dlistint_s
 } dlistint_t;
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 size_t dlistint_len(const dlistint_t *h);
+size_t listint_len(dlistint_t *h);
 size_t print_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
