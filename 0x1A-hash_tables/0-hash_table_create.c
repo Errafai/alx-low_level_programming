0e5c48f6a0bc2a0f0e4210170757256ab523d07e
Errafai <errafaiachraf1923@gmail.com>
2024-02-16 23:10:57 +0100

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new == NULL)
 		return (NULL);
 	new->size = size;
-	new->array = (hash_node_t **) malloc(sizeof(hash_node_t) * new->size);
+	new->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * new->size);
 	if (new->array == NULL)
 	{
 		free(new);
