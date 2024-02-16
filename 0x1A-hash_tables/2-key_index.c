1e55a9a3958789c3a65c02e999ce1a04ee8834a7
Errafai <errafaiachraf1923@gmail.com>
2024-02-17 00:13:19 +0100

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,10 @@
 #include "hash_tables.h"
+/**
+ * key_index - return the index of the key
+ * @size: the size of the table
+ * @key: the key of the element
+ * Return: the index of the key to put it in
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
