2f78622dfe955d758eb7e3dff9fe75964c1d3a1f
Errafai <errafaiachraf1923@gmail.com>
2024-02-16 22:59:14 +0100

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new = malloc(sizeof(hash_table_t));
+	hash_table_t *new = (hash_table_t*) malloc(sizeof(hash_table_t));
 	size_t i;
 	if (new == NULL)
 		return NULL;
 	new->size = size;
-	new->array = malloc(sizeof(hash_node_t) * new->size);
+	new->array = (hash_node_t**) malloc(sizeof(hash_node_t) * new->size);
 	if (new->array == NULL)
 		return NULL;
 	for (i = 0; i < new->size; i++)
