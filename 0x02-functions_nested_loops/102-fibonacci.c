e4b0e1de4b795d15ee70460626a5d9bc13210e00
Errafai <errafaiachraf1923@gmail.com>
2024-02-18 03:49:53 +0100

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - print the first 50 fibonachi number
+ * Return: always 0
+ */
+int main(void)
+{
+	long int fibn_1 = 1, fibn_2 = 0, fibn = 0, i = 0;
+
+	while (i <= 50)
+	{
+		fibn = fibn_1 + fibn_2;
+		printf("%ld", fibn);
+		if (i != 50)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
+		fibn_2 = fibn_1;
+		fibn_1 = fibn;
+	}
+	return (0);
+}
