6150895a2355d060d45ca7969820a35245cd35d0
Errafai <errafaiachraf1923@gmail.com>
2024-02-18 22:47:47 +0100

    4-print_most_number.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_most_numbers - print all numbers from 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		if (i != 2 && i != 4)
+			_putchar('0' + i);
+	_putchar('\n');
+}
