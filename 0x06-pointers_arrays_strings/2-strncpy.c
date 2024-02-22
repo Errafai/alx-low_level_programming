a054bfcdfdfe17d73161510bab1838f09f6b5230
Errafai <errafaiachraf1923@gmail.com>
2024-02-22 01:00:02 +0100

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
 
 	while (i < n && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (src[j])
-		j++;
-	if (j < n)
+	if (i < n)
 		dest[i] = '\0';
 	return (dest);
 }
