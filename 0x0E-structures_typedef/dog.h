8200de4b8c7a4ff18cc78d9f60d75ac8467d546a
Errafai <errafaiachraf1923@gmail.com>
2024-03-09 15:51:36 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,15 +9,15 @@
  * @owner: the owner of the dog
  * Description: trying to learn struct from this example
  */
-struct dog
+typedef struct dog
 {
 		char *name;
 		float age;
 		char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _putchar(char c);*/
+int _putchar(char c);
 #endif
