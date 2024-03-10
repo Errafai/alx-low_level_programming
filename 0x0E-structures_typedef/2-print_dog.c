a4508880eb0c4d04790500038ff0f4a2f9ce8467
Errafai <errafaiachraf1923@gmail.com>
2024-03-10 21:42:52 +0000

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,6 @@ void print_dog(struct dog *d)
 		if (d == NULL)
 			return;
 		printf("Name: %s\n", d->name);
-		printf("Age: %.1f\n", d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
 }
