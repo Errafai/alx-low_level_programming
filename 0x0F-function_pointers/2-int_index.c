ac488cc0d833acc3383dbf08d328b7b2d6220a9a
Errafai <errafaiachraf1923@gmail.com>
2024-03-21 15:35:33 +0000

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 		int i;
 
 		if (size <= 0)
-				return -1;
+			return (-1);
 		for (i = 0; i < size; i++)
 		{
-				if (cmp(array[i]))
-						return (i);
+			if (cmp(array[i]))
+				return (i);
 		}
 		return (-1);
 }
