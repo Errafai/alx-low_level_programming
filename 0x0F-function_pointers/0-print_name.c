7b110a52f8b463a1e3cf02194ddd7b2f3fa1421d
Errafai <errafaiachraf1923@gmail.com>
2024-03-21 10:17:27 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,10 @@
+#include "function_pointers.h"
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: function pointer
- * Return: void
+ * print_name - print the name string in the function
+ * @name: the name string
+ * @f: the function to print the name sting
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
 		f(name);
 }
